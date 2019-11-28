895f0ac23018e0ec8806d3b022ec6d2e84ce412f
朱志伟 <zhuzhiwei_1984@outlook.com>
2019-11-28 12:50:40 +0800

    完成部分书籍

--- 主程序/app/视图_家庭书屋.py
+++ 主程序/app/视图_家庭书屋.py
@@ -1,9 +1,12 @@
-from . import 全_蓝图路由
+from . import 全_蓝图路由, 全_数据库
 from flask import render_template
 
 # 主页_路由 = Blueprint('主页_路由',__name__, static_folder='static', template_folder='templates')
+局_书籍数据库游标 = 全_数据库['zzw']
 
 @全_蓝图路由.route('/家庭书屋')
 def 视图_家庭书屋():
+    a = 局_书籍数据库游标.list_collection_names()
+    print(a)
     return render_template('家庭书屋.html')
     # return "大家好"
